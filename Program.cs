a1524781c6bd1ad84f962592dacb5284723098e5
MaxDean-NZ <ac157646@avcol.school.nz>
2026-05-25 15:11:18 +1200

    I added a method to prompt you which difficulty to choose.

--- Program.cs
+++ Program.cs
@@ -5,11 +5,22 @@
         static void Main(string[] args)
         {
             String name;
+            char difficulty;
+
             Console.WriteLine("Please enter your name: ");
             name = Console.ReadLine();
             Console.WriteLine($"Welcome to the quiz, {name}!");
 
-            
+            difficulty = SelectDifficulty();
+            Console.WriteLine($"you chose {difficulty} ");
+        }
+        static char SelectDifficulty()
+        {
+            char difficulty;
+            Console.WriteLine("Would you like to play \"E\" Easy, \"M\" Medium, or \"H\" Hard?");
+            difficulty = Convert.ToChar(Console.ReadLine());
+            return difficulty;
+
         }
     }
 }
