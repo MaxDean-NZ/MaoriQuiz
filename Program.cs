fa63380c1bd275dfca74a60bfff90816399485d8
MaxDean-NZ <ac157646@avcol.school.nz>
2026-05-26 14:14:25 +1200

    I added some comments, and added some input validation for the name input, forcing you to retype if its null, empty, or greater or less than 20-3 characters

--- Program.cs
+++ Program.cs
@@ -7,8 +7,16 @@
             String name;
             String quizDifficulty;
 
-            Console.WriteLine("Please enter your name: ");
-            name = Console.ReadLine().Trim();
+            do
+            {
+                Console.WriteLine("Please enter your name: ");
+                name = Console.ReadLine().Trim();
+                // checks whether is null or empty
+                if (name == null || name == "") Console.WriteLine("Name is not allowed to be empty.\n");
+                // checks whether it obeys lower and upper bound
+                if (name.Length > 20 || name.Length < 3) Console.WriteLine("Name has to be between 20 and 3 characters.\n"
+            } while(name == null || name == "");
+
             Console.WriteLine($"Welcome to the quiz, {name}!");
 
             quizDifficulty = SelectDifficulty();
@@ -21,9 +29,10 @@
             {
                 Console.WriteLine("Would you like to play \"E\" Easy, \"M\" Medium, or \"H\" Hard?");
                 quizDifficulty = Console.ReadLine().Trim().ToUpper();
-                if (quizDifficulty != "E" && quizDifficulty != "M" && quizDifficulty != "H") Console.WriteLine("Invalid input. Please enter E, M, or H");
+                // checks if input is valid
+                if (quizDifficulty != "E" && quizDifficulty != "M" && quizDifficulty != "H") Console.WriteLine("Invalid input. Please enter E, M, or H\n");
             } while (quizDifficulty != "E" && quizDifficulty != "M" && quizDifficulty != "H");
-            
+
             if (quizDifficulty == "E") Environment.Exit(0);
             return quizDifficulty;
 
